22dc4e3795c5f4e7ba485babbf74b2ed65e0b13b
nevzattalhaozcan <ozcann.talha@gmail.com>
2023-04-13 11:06:49 +0300

    initial commit

---
+++ src/test/java/com/library/runners/CukesRunner.java
@@ -0,0 +1,21 @@
+package com.library.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "html:target/html-report.html",
+                "json:target/json-report.json",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber",
+                "rerun:target/rerun.txt"
+        },
+        features = "src/test/resources/features",
+        glue = "com/library/step_definitions",
+        tags = "",
+        dryRun = false
+)
+public class CukesRunner {
+}
---
+++ src/test/java/com/library/utils/Driver.java
@@ -0,0 +1,77 @@
+package com.library.utils;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class Driver {
+
+    private Driver(){}
+
+    private static WebDriver driver;
+
+    public static WebDriver getDriver(){
+
+        if(driver == null){
+
+            String browser = ConfigurationReader.getProperty("browser");
+
+            switch (browser){
+
+                case "chrome":
+                    ChromeOptions options = new ChromeOptions();
+                    options.addArguments("--remote-allow-origins=*");
+                    options.addArguments("--lang=en-US");
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver(options);
+                    driver.manage().window().maximize();
+                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    break;
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    driver.manage().window().maximize();
+                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    break;
+                case "edge":
+                    if (System.getProperty("os.name").toLowerCase().contains("mac")) {
+                        throw new WebDriverException("Your operating system does not support the requested browser");
+                    }
+                    WebDriverManager.edgedriver().setup();
+                    driver = new EdgeDriver();
+                    driver.manage().window().maximize();
+                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    break;
+                case "safari":
+                    if (System.getProperty("os.name").toLowerCase().contains("windows")) {
+                        throw new WebDriverException("Your operating system does not support the requested browser");
+                    }
+                    WebDriverManager.getInstance(SafariDriver.class).setup();
+                    driver = new SafariDriver();
+                    driver.manage().window().maximize();
+                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    break;
+            }
+
+        }
+
+        return driver;
+    }
+
+    public static void closeDriver(){
+
+        if(driver != null){
+            driver.quit();
+            driver = null;
+        }
+
+    }
+
+}
